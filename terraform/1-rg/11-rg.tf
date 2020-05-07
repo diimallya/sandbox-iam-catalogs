bfa767d2ce72d4be469d220be3c98bb3b60e0138
GandhiCloudLab <GandhiCloudLab@gmail.com>
2020-05-07 19:53:40 +0530

    rg

--- terraform/1-rg/11-rg.tf
+++ terraform/1-rg/11-rg.tf
@@ -1,13 +1,13 @@
 //create all resource groups by taking input from 01-resource-group.json
 resource "ibm_resource_group" "rg1" {
-  	name  = "appdev-cloudnative"
+  	name  = "g1-appdev-cloudnative"
 }
-# resource "ibm_resource_group" "rg2" {
-#   	name  = "cloud-management"
-# }
-# resource "ibm_resource_group" "rg3" {
-#   	name  = "business-data"
-# }
-# resource "ibm_resource_group" "rg4" {
-#   	name  = "business-automation"
-# }
+resource "ibm_resource_group" "rg2" {
+  	name  = "g1-cloud-management"
+}
+resource "ibm_resource_group" "rg3" {
+  	name  = "g1-business-data"
+}
+resource "ibm_resource_group" "rg4" {
+  	name  = "g1-business-automation"
+}
