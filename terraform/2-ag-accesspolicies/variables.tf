4a2a66ba23447c40b7d0f85bb8d1c12a7f57a115
diimallya <divya.mallya86@gmail.com>
2020-05-08 18:57:28 +0530

    updated access-groups

--- terraform/2-ag-accesspolicies/variables.tf
+++ terraform/2-ag-accesspolicies/variables.tf
@@ -9,12 +9,12 @@ variable developer_id{
 
 variable admin_access_groups{
 	type = "list"
-	default = ["CLOUDMANAGEMENT-ADMINS", "DATA-ADMINS", "APPDEV-ENVIRONMENT-ADMINS"]
+	default = ["CLOUDMANAGEMENT-ADMINS", "DATA-ADMINS", "APPDEV-ENVIRONMENT-ADMINS", "APPDEV-ADMINS", "INTEGRATION-ADMINS", "BUSINESS-AUTOMATION-ADMINS" ]
 }
 
 variable admin_resource_groups{
 	type = "list"
-	default = ["cloud-management", "business-data", "appdev-cloudnative"]
+	default = ["cloud-management", "business-data", "appdev-cloudnative", "appdev-cloudnative", "appdev-cloudnative", "business-automation"]
 }
 
 variable user_access_groups{
