c965b5d2e3afcdd328d7587d7f806b1a037e34d6
GandhiCloudLab <GandhiCloudLab@gmail.com>
2020-05-08 10:56:08 +0530

    Script Updated

--- terraform/8-accesspolicy/11-ag-admin.tf
+++ terraform/8-accesspolicy/11-ag-admin.tf
@@ -5,7 +5,7 @@ data "ibm_iam_access_group" "res_ag_1" {
 
 resource "ibm_iam_access_group_policy" "res_ag_policy1" {
   
-  access_group_id = "${ibm_iam_access_group.res_ag_1.id}"
+  access_group_id = "${data.ibm_iam_access_group.res_ag_1.id}"
   roles        = ["Administrator", "Manager"]
   resources  {
     service   = "container-registry"
