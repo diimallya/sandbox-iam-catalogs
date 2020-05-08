d63fe10168331789898bbdc35f5bbb2ad647d757
GandhiCloudLab <GandhiCloudLab@gmail.com>
2020-05-08 11:28:49 +0530

    Script Updated

--- install/1-create-tgz.sh
+++ install/1-create-tgz.sh
@@ -15,7 +15,7 @@ rm -rf .git .gitignore
 COPYFILE_DISABLE=1 tar czfv 1-rg-0.0.1.tgz 1-rg/
 COPYFILE_DISABLE=1 tar czfv 2-ag-accesspolicies-0.0.1.tgz 2-ag-accesspolicies/
 COPYFILE_DISABLE=1 tar czfv 8-ag-0.0.1.tgz 8-ag/
-COPYFILE_DISABLE=1 tar czfv 8-accesspolicy-0.0.1.tgz 8-accesspolicy/
+COPYFILE_DISABLE=1 tar czfv 8-ag-accesspolicy-0.0.1.tgz 8-ag-accesspolicy/
 COPYFILE_DISABLE=1 tar czfv 9-ag-0.0.1.tgz 9-ag/
 
 mv *.tgz ../tgz
