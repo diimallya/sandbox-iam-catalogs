6cd56ded13e509e99c22d6797af26b6afe499881
diimallya <divya.mallya86@gmail.com>
2020-05-11 20:06:34 +0530

    Script Updated

--- terraform/3-ag-users/variables.tf
+++ terraform/3-ag-users/variables.tf
@@ -10,61 +10,81 @@ variable user_access_groups{
 
 variable admin_id_1{
     type = "string"
+    default = ""
 }
 variable admin_id_2{
     type = "string"
+     default = ""
 }
 variable admin_id_3{
     type = "string"
+     default = ""
 }
 variable admin_id_4{
     type = "string"
+     default = ""
 }
 variable admin_id_6{
     type = "string"
+     default = ""
 }
 variable admin_id_7{
     type = "string"
+     default = ""
 }
 variable admin_id_8{
     type = "string"
+     default = ""
 }
 variable admin_id_9{
     type = "string"
+     default = ""
 }
 variable admin_id_10{
     type = "string"
+     default = ""
 }
 variable admin_id_5{
     type = "string"
+     default = ""
 }
 variable user_id_1{
     type = "string"
+     default = ""
 }
 variable user_id_2{
     type = "string"
+     default = ""
 }
 variable user_id_3{
     type = "string"
+     default = ""
 }
 variable user_id_4{
     type = "string"
+     default = ""
 }
 variable user_id_5{
     type = "string"
+     default = ""
 }
 variable user_id_6{
     type = "string"
+     default = ""
 }
 variable user_id_7{
     type = "string"
+     default = ""
 }
 variable user_id_8{
     type = "string"
+     default = ""
 }
 variable user_id_9{
     type = "string"
+     default = ""
 }
 variable user_id_10{
     type = "string"
+     default = ""
 }
