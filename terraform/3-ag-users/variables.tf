aeecef06d50558d754622f1f5cd14cb8af8c27cb
diimallya <divya.mallya86@gmail.com>
2020-05-12 16:42:02 +0530

    Script Updated

--- terraform/3-ag-users/variables.tf
+++ terraform/3-ag-users/variables.tf
@@ -8,83 +8,9 @@ variable user_access_groups{
 	default = ["APPDEV-USERS"]
 }
 
-variable admin_id_1{
+variable user_ids{
     type = "string"
-    default = ""
 }
-variable admin_id_2{
+variable admin_ids{
     type = "string"
-     default = ""
-}
-variable admin_id_3{
-    type = "string"
-     default = ""
-}
-variable admin_id_4{
-    type = "string"
-     default = ""
-}
-variable admin_id_6{
-    type = "string"
-     default = ""
-}
-variable admin_id_7{
-    type = "string"
-     default = ""
-}
-variable admin_id_8{
-    type = "string"
-     default = ""
-}
-variable admin_id_9{
-    type = "string"
-     default = ""
-}
-variable admin_id_10{
-    type = "string"
-     default = ""
-}
-variable admin_id_5{
-    type = "string"
-     default = ""
-}
-variable user_id_1{
-    type = "string"
-     default = ""
-}
-variable user_id_2{
-    type = "string"
-     default = ""
-}
-variable user_id_3{
-    type = "string"
-     default = ""
-}
-variable user_id_4{
-    type = "string"
-     default = ""
-}
-variable user_id_5{
-    type = "string"
-     default = ""
-}
-variable user_id_6{
-    type = "string"
-     default = ""
-}
-variable user_id_7{
-    type = "string"
-     default = ""
-}
-variable user_id_8{
-    type = "string"
-     default = ""
-}
-variable user_id_9{
-    type = "string"
-     default = ""
-}
-variable user_id_10{
-    type = "string"
-     default = ""
 }
