ba2d46063aee36bf6c4b1c7b015f8c1eb0de74b1
diimallya <divya.mallya86@gmail.com>
2020-05-13 13:24:41 +0530

    Script Updated

--- terraform/3-ag-users/variables.tf
+++ terraform/3-ag-users/variables.tf
@@ -1,8 +1,9 @@
 variable admin_access_groups{
 	default = "CLOUDMANAGEMENT-ADMINS,DATA-ADMINS,APPDEV-ENVIRONMENT-ADMINS,APPDEV-ADMINS,INTEGRATION-ADMINS,BUSINESS-AUTOMATION-ADMINS"
+}
 
 variable user_access_groups{
-	default = "APPDEV-USERS"
+	default = "APPDEV-USERS-CUSTOM"
 }
 
 variable user_ids{
